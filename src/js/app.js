68fe823ec1294529ce9e7e950816de764583bbb4
Sofia Araujo Areal <163517593+Sofia28062001@users.noreply.github.com>
2024-06-11 21:18:32 +0000

    First Commit

--- src/js/app.js
+++ src/js/app.js
@@ -34,7 +34,7 @@ function render(variables = {}) {
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1> ${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
+          <h2>${variables.role} </h2>
           <h3>${variables.city}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
